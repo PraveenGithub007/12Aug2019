b4bd4adf19096e12fb4c3961814b8e7d1ebeaa57
Praveen N <praveen.nampally@honeywell.com>
2019-08-12 20:38:28 +0530

    100SSSSSSSSSS1

--- src/app.js
+++ src/app.js
@@ -109,5 +109,5 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT | 3000;
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
+    console.log('Server is up on port ')
 })
